85e99ec538ca86484d6900f9ca89abb879d8b1ce
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-05-23 15:40:00 +0200

    createStore function
    The function is responsible to set up the store, it includes the state and dispatching actions and then the subscribe the state changes
    
    the dispatch method updates the state by sending the action tothe reducer, action is the object that describes the change to be made
    
    The getState returns the current state of the story

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-// Reducer function to manage state transitions
 function counter(state = 0, action) {
-  switch (action.type) {
+  switch (
+    action.type //decides how to update the state based on the action that was dispatched.
+  ) {
     case "INCREMENT":
       return state + 1;
     case "DECREMENT":
       return state - 1;
     case "RESET":
       return 0;
-    default:
+    default: //When the action type does not match the statements in the switch block, it executes and returns to default state
       return state;
   }
 }
+// function a new store that manages the state of the application using a reducer function.
+function createStore(reducer) {
+  let state; //holds the current state of the store
+  let listeners = []; //holds function that will be called whenever the state changes.
+
+  // Get the current state
+  const getState = () => state; //returns current state of the store
+
+  // Dispatch an action to update the state
+  const dispatch = (action) => {
+    //used to updte the state by sending an action to reducer
+    state = reducer(state, action); //
+    listeners.forEach((listener) => listener());
+  };
+
+ 
+
+ 
